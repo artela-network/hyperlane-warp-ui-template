13be761573bc08be3b05a1187326576f44f2362d
linus <1748501327@qq.com>
2024-11-08 14:43:08 +0800

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,7 +31,7 @@ export default function Document() {
         <meta property="og:image" content={`${APP_URL}/logo.png`} />
         <meta property="og:description" content={APP_DESCRIPTION} />
       </Head>
-      <body className={`${MAIN_FONT.variable} font-sans text-black`}>
+      <body className={`${MAIN_FONT.className} font-sans text-black`}>
         <Main />
         <NextScript />
       </body>
