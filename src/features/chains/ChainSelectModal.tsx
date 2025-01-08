4bf0ddd7f7f34e77b596db536ea778a2c3dc1e55
linus <1748501327@qq.com>
2025-01-08 15:53:30 +0800

    change ui of dialog

--- src/features/chains/ChainSelectModal.tsx
+++ src/features/chains/ChainSelectModal.tsx
@@ -1,10 +1,7 @@
 import { useMemo } from 'react';
 
 import { ChainLogo } from '../../components/icons/ChainLogo';
-import { Modal } from '../../components/layout/Modal';
-
 import { getChainDisplayName } from './utils';
-
 export function ChainSelectListModal({
   isOpen,
   close,
@@ -24,21 +21,40 @@ export function ChainSelectListModal({
   };
 
   const sortedChains = useMemo(() => chains.sort(), [chains]);
+  console.log(sortedChains);
+  if (!isOpen) return null;
 
   return (
-    <Modal isOpen={isOpen} title="Select Chain" close={close}>
-      <div className="mt-2 flex flex-col space-y-1">
-        {sortedChains.map((c) => (
-          <button
-            key={c}
-            className="flex items-center rounded px-2 py-1.5 text-sm transition-all duration-200 hover:bg-gray-100 active:bg-gray-200"
-            onClick={onSelectChain(c)}
+    <div className="fixed inset-0 z-30">
+      <div className="fixed inset-0 bg-black/25 backdrop-blur-xl" onClick={close} />
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div
+            className="w-full max-w-xl transform overflow-hidden rounded-3xlp-8 text-left align-middle"
+            onClick={(e) => e.stopPropagation()}
           >
-            <ChainLogo chainName={c} size={16} background={false} />
-            <span className="ml-2">{getChainDisplayName(c, true)}</span>
-          </button>
-        ))}
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center justify-center text-xl font-medium leading-6 text-center mb-6 bg-[#FFFFFF1A] w-[182px] h-[56px] rounded-full">
+                <span className="text-white">Select network</span>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {sortedChains.map((c) => (
+                <button
+                  key={c}
+                  className="flex flex-col items-center justify-center rounded-2xl w-[172px] h-[211px] bg-[#0000c9]"
+                  onClick={onSelectChain(c)}
+                >
+                  <div className="w-12 h-12 flex items-center justify-center">
+                    <ChainLogo chainName={c} size={80} background={false} />
+                  </div>
+                  <span className="mt-3 text-sm text-white">{getChainDisplayName(c, true)}</span>
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-    </Modal>
+    </div>
   );
 }
