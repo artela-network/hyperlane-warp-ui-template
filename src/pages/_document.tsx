e96ce008c0e13f05868c029df8086d650c282623
linus <1748501327@qq.com>
2025-01-18 15:28:57 +0800

    add clairy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,19 @@ export default function Document() {
     <Html>
       <Head>
         <meta charSet="utf-8" />
-
+        <head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                 (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "pvpx2ncir7");
+              `,
+            }}
+          />
+        </head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
